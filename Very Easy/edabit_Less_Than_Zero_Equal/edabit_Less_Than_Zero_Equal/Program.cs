cc4a4bba46f7237c728b486ca767e04a8be0baa9
Benjamin Lafever <52665892+Benjamin-Lafever@users.noreply.github.com>
2020-01-13 14:21:36 -0500

    Add files via upload

---
+++ Very Easy/edabit_Less_Than_Zero_Equal/edabit_Less_Than_Zero_Equal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edabit_Less_Than_Zero_Equal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool true_or_false = false;
+
+            Console.WriteLine("Enter a number and if it is less than or equal to 0 true will be displayed: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if(number <= 0)
+            {
+                true_or_false = true;
+                Console.WriteLine("The answer is {0}", true_or_false);
+                Console.ReadKey();
+            }
+            else
+            {
+                true_or_false = false;
+                Console.WriteLine("The answer is {0}", true_or_false);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Very Easy/edabit_Next_Number/edabit_Next_Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace edabit_Next_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be incremented: ");
+            int incremented = Convert.ToInt32(Console.ReadLine());
+            incremented += 1;
+            Console.WriteLine(incremented);
+            Console.ReadKey();
+        }
+    }
+}
